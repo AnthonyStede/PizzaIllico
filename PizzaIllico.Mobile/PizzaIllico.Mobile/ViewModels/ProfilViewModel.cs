18624bbeeacd0064cb53ce121c394fe4a17132bd
LingoZHANG <gintoki.chel@gmail.com>
2021-03-28 05:34:47 +0200

    add some class

---
+++ PizzaIllico.Mobile/PizzaIllico.Mobile/ViewModels/ProfilViewModel.cs
@@ -0,0 +1,21 @@
+﻿using PizzaIllico.Mobile.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaIllico.Mobile.ViewModels
+{
+    class ProfilViewModel
+    {
+
+        private ApiService _apiService = new ApiService();
+        public string Email { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Message { get; set; }
+
+
+
+    }
+}
